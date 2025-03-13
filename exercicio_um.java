915588d4340060cedfa1218de902738d7ce55b44
vai leo <leonardotaqua48@gmail.com>
2025-03-12 23:37:17 -0300

    exercicio um

--- exercicio_um.java
+++ exercicio_um.java
@@ -4,6 +4,38 @@ public class exercicio_um {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Digite um numero de 1 a 7 : ");
+        int dia = sc.nextInt();
+
+        switch (dia) {
+
+            case 1:
+                System.out.println("Domingo");
+                break;
+            case 2:
+                System.out.println("Segunda");
+                break;
+            case 3:
+                System.out.println("Terça-feira");
+                break;
+            case 4:
+                System.out.println("Quarta");
+                break;
+            case 5:
+                System.out.println("Quinta");
+                break;
+            case 6:
+                System.out.println("Sexta");
+                break;
+            case 7:
+                System.out.println("Sabado");
+                break;
+            default:
+                System.out.println("Nenhum numero equivalente selecionado");
+
+
+        }
+
 
     }
 
